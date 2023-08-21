a99c601fbb3d63db461fde29d4a86b0993b3612d
eyadrid <dridii.eya80@gamil.com>
2023-08-21 21:41:51 +0100

    App.js

--- src/frontend/Components/Login/index.js
+++ src/frontend/Components/Login/index.js
@@ -1,30 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState} from 'react';
 import { Form, Input, Button, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthContext';
+import { LoginService } from "../../service/login/AuthService"
 import '../Login/index.css';
 
 const Login = () => {
   const navigate = useNavigate();
-  const { toggleLogin } = useContext(AuthContext);
-  
-  const onFinish = (values) => {
-    console.log('Form values:', values);
-    toggleLogin();
-    navigate('/PageContent');
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChangeEmail = (event) =>{
+      setData({
+          ...data,
+          email: event.target.value
+      })
   };
-  const handleSignUpClick = () => {
-    navigate('/Signup/index');
+  const handleChangePassword = (event) =>{
+      setData({
+          ...data,
+          password: event.target.value
+      })
   };
+  const handleSubmit = (e)=>{
+      e.preventDefault()
+      LoginService(data).then((res)=>{
+          localStorage.setItem('token', res.data.accessToken);
+          localStorage.setItem('refreshToken', res.data.refreshToken);
+          localStorage.setItem('user', JSON.stringify(res.data.userEntityDTO));
+          navigate('/dashboard');
+
+      }).catch((e)=>{
+        console.log(e);
+      })
+    }
+    const { toggleLogin } = useContext(AuthContext);
+    
+    const onFinish = (values) => {
+      console.log('Form values:', values);
+      toggleLogin();
+      navigate('/PageContent');
+    };
+    const handleSignUpClick = () => {
+      navigate('/Signup/index');
+    };
 
   return (
     <div className='loginfo'>
       <Form name="login-form" onFinish={onFinish}>
         <Typography.Title>Login</Typography.Title>
         <Form.Item
-          label="User Name"
-          name="username"
+          label="Email"
+          name="email"
           rules={[{ required: true, message: 'Ce champ est obligatoire' }]}
+          onChange={handleChangeEmail}
         >
           <Input />
         </Form.Item>
@@ -32,11 +63,12 @@ const Login = () => {
           label="Password"
           name="password"
           rules={[{ required: true, message: 'Ce champ est obligatoire' }]}
+          onChange={handleChangePassword}
         >
           <Input.Password />
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit">Login</Button>
+          <Button onClick={handleSubmit} type="primary" >Login</Button>
           <Button type="primary" onClick={handleSignUpClick} style={{ marginLeft: '20px' }}>
             SignUp
           </Button>
