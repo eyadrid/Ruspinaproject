e86613bd9b0da473b700232347a501728898c762
Mohamed El Mahdi Trabelsi <62462645+DevMahdiTR@users.noreply.github.com>
2023-09-11 16:49:09 +0100

    Delete src directory

--- src/frontend/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as DashBoard} from './admin/dashboard';
-export {default as Home} from './home/Home';
-export {default as Contact} from './contact/Contact';
-export {default as About} from './about/About';
